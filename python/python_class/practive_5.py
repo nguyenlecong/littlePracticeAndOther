3586a9694804879c4ff0658d6344e2623dc60af4
nguyenlc <nguyen.lc41@gmail.com>
2023-06-04 22:13:41 +0700

    getter, setter

--- python/python_class/practive_5.py
+++ python/python_class/practive_5.py
@@ -1,19 +1,21 @@
 class SieuNhan:
     suc_manh = 50
+
     def __init__(self, ten) -> None:
         self.ten = ten
 
-    @property  # getter
+    @property  # getter # trở thành một thuộc tính (property~thuoc tinh)
     def in_ten(self):
         print('ten toi la', self.ten)
 
-    @in_ten.setter
+    @in_ten.setter  # Bản chất là một phương thức, không thể gán ngang cho một giá trị -> setter
     def in_ten(self, ten_moi):
         self.ten = ten_moi
 
-    @in_ten.deleter
+    @in_ten.deleter  # Nếu bạn muốn dùng xong xóa
     def in_ten(self):
         self.ten = None
+
 
 A = SieuNhan('do')
 A.in_ten
