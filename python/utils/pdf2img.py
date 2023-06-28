2c295e40c79a0d68d26bf3e4ab1311e3eeccdeb2
Le Cong Nguyen <62042231+nguyenlecong@users.noreply.github.com>
2023-06-28 15:01:00 +0700

    Update pdf2img.py

--- python/utils/pdf2img.py
+++ python/utils/pdf2img.py
@@ -4,9 +4,10 @@ import numpy as np
 
 pdf_path = ''
 
-images = pdf2image.convert_from_path(pdf_path)
+images = pdf2image.convert_from_path(pdf_path, dpi=500)
 
 for i, image in enumerate(images):
-    image = np.array(image)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    cv2.imwrite(f'{i+1}.jpg', image)
+    image.save(f'{i+1}.jpg', 'JPEG')
+    # image = np.array(image)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # cv2.imwrite(f'{i+1}.jpg', image)
