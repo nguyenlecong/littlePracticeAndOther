2f738f1707c1b04e78934fa73a11cc47417ed1d6
Le Cong Nguyen <62042231+nguyenlecong@users.noreply.github.com>
2023-06-29 18:03:44 +0700

    pdf2image, faster pymudf

--- python/utils/pdf2img.py
+++ python/utils/pdf2img.py
@@ -1,13 +1,42 @@
-import cv2
-import pdf2image
-import numpy as np
+import time
 
-pdf_path = ''
 
-images = pdf2image.convert_from_path(pdf_path, dpi=500)
+def use_pdf2image(pdf_path):
+        import pdf2image
 
-for i, image in enumerate(images):
-    image.save(f'{i+1}.jpg', 'JPEG')
-    # image = np.array(image)
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # cv2.imwrite(f'{i+1}.jpg', image)
+        pages = pdf2image.convert_from_path(pdf_path, dpi=600)
+
+        for i, page in enumerate(pages):
+            dst_filename = f'pdf2image_{i}.png'
+
+            page.save(dst_filename, format='PNG')
+
+
+# Faster
+def use_pymupdf(pdf_path):
+    import fitz
+
+    pages = fitz.open(pdf_path)
+
+    for page in pages:
+        pix = page.get_pixmap()
+
+        pix.save(f'pymupdf_{page.number}.png')
+
+
+def main(func):
+    pdf_path = '''
+    
+    start = time.time()
+
+    func(pdf_path)
+
+    end = time.time()
+    
+    process_time = str(round((end-start)*1000, 3)) + ' ms'
+    print(process_time)
+
+
+if __name__ == '__main__':
+    main(use_pdf2image)
+    main(use_pymupdf)
